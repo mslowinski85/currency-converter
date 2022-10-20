72976ab243ebf0db8a306f47f6f6ecdaea54f66e
Marek Slowinski <marekslowinski@gmail.com>
2022-10-20 20:24:41 +0200

    added functions to js file

--- js/script.js
+++ js/script.js
@@ -1,36 +1,60 @@
-let inputElement = document.querySelector(".js-sum");
-let formElement = document.querySelector(".js-form");
-let inputElementCurrentRate = document.querySelector(".js-currentRate");
-let radioElementDollar = document.querySelector(".js-dollar");
-let radioElementEuro = document.querySelector(".js-euro");
-let radioElementPound = document.querySelector(".js-pound");
-let radioElementFrank = document.querySelector(".js-frank");
-let inputElementFinallySum = document.querySelector(".js-finallySum");
+{
+  const welcome = () => {
+    console.log(
+      "Hej, tu Marek Słowiński :) Witaj na kursie Frontend Developer"
+    );
+  };
 
-formElement.addEventListener("input", () => {
-  if (inputElement.value >= 0) {
+  const inputElement = () => {
+    return (element = document.querySelector(".js-sum"));
+  };
+
+  const updateResultInput = (finallySum) => {
+    const inputElementFinallySum = document.querySelector(".js-finallySum");
+    inputElementFinallySum.value = `${finallySum.toFixed(2)} zł`;
+  };
+  
+  const currentRateValue = (choosingRate) => {
+    const inputElementCurrentRate = document.querySelector(".js-currentRate");
+    const finallySum = choosingRate * inputElement().value;
+
+    inputElementCurrentRate.value = choosingRate;
+    updateResultInput(finallySum);
+  };
+
+  const radioElementChecked = () => {
+    const radioElementDollar = document.querySelector(".js-dollar");
+    const radioElementEuro = document.querySelector(".js-euro");
+    const radioElementPound = document.querySelector(".js-pound");
+
     if (radioElementDollar.checked === true) {
-      let dollarRate = 4.9569;
-      inputElementCurrentRate.value = dollarRate;
-      let finallySum = dollarRate * inputElement.value;
-      inputElementFinallySum.value = `${finallySum.toFixed(2)} zł`;
+      const dollarRate = 4.9569;
+      currentRateValue(dollarRate);
     } else if (radioElementEuro.checked === true) {
-      let euroRate = 4.833;
-      inputElementCurrentRate.value = euroRate;
-      let finallySum = euroRate * inputElement.value;
-      inputElementFinallySum.value = `${finallySum.toFixed(2)} zł`;
+      const euroRate = 4.833;
+      currentRateValue(euroRate);
     } else if (radioElementPound.checked === true) {
-      let poundRate = 5.5886;
-      inputElementCurrentRate.value = poundRate;
-      let finallySum = poundRate * inputElement.value;
-      inputElementFinallySum.value = `${finallySum.toFixed(2)} zł`;
+      const poundRate = 5.5886;
+      currentRateValue(poundRate);
     } else {
-      let frankRate = 4.9526;
-      inputElementCurrentRate.value = frankRate;
-      let finallySum = frankRate * inputElement.value;
-      inputElementFinallySum.value = `${finallySum.toFixed(2)} zł`;
+      const frankRate = 4.9526;
+      currentRateValue(frankRate);
     }
-  } else {
-    inputElement.value = "";
-  }
-});
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    welcome();
+    inputElement();
+    formElement.addEventListener("input", () => {
+      if (inputElement().value >= 0) {
+        radioElementChecked();
+      } else {
+        inputElement().value = "";
+      }
+    });
+  };
+
+  init();
+}
